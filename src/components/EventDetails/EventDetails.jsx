bdc70f60b9a6df34859415f91727d3acc8da7c99
ali <alihasansaad1@gmail.com>
2025-08-13 13:18:39 +0300

    event details updated

--- src/components/EventDetails/EventDetails.jsx
+++ src/components/EventDetails/EventDetails.jsx
@@ -1,43 +1,99 @@
-import { useParams } from 'react-router-dom'
+
+import { useParams, Link } from 'react-router-dom'
 import { useEffect, useState } from 'react'
 import * as eventService from '../../services/eventService'
-import { Link } from 'react-router-dom'
+import * as attendanceService from '../../services/attendanceService'
 
 const EventDetails = (props) => {
 
   const { eventId } = useParams()
 
   const [event, setEvent] = useState()
+  const [attendees, setAttendees] = useState([])
+  const [attending, setAttending] = useState(false)
 
   useEffect(() => {
-    // fetch a single event
     const fetchEvent = async () => {
-      // call the event service
       const eventData = await eventService.show(eventId)
-      console.log(eventData)
       setEvent(eventData)
     }
     fetchEvent()
   }, [eventId])
+
+  useEffect(() => {
+   
+      const fetchAttendees = async () => {
+        const data = await attendanceService.list(eventId)
+        if (data && data.attendees) {
+          setAttendees(data.attendees)
+        } else {
+          setAttendees([])
+        }
+      }
+      fetchAttendees()
+      setAttending(false) 
+  
+      setAttendees([]) 
+  
+  }, [event, eventId, props.user])
+
   if (!event) return <main>Loading...</main>
+
+  let isOwner = false
+  if (props.user && event.owner && event.owner._id === props.user._id) {
+    isOwner = true
+  }
+
+  const handleToggleAttend = async () => {
+    if (!props.user) return
+    if (isOwner) return 
+    try {
+      if (attending) {
+        await attendanceService.leave(eventId)
+        setAttending(false)
+      } else {
+        await attendanceService.join(eventId)
+        setAttending(true)
+      }
+    } catch (err) {
+      console.log(err)
+    }
+  }
+
   return (
     <main>
       <header>
-        {/* <p>{event.category.toUpperCase()}</p> */}
         <h1>{event.title}</h1>
         <p>
-          {event.owner.username} posted on {event.data}
+          {event.owner && event.owner.username} posted on{" "}
+          {event.date ? new Date(event.date).toLocaleDateString() : ""}
         </p>
         <p>{event.description}</p>
 
-        {event.owner._id === props.user._id && (
+        {isOwner && (
           <>
-            <Link className='edit-btn' to={`/events/${eventId}/edit`}>Edit</Link>
+            <Link className="edit-btn" to={`/events/${eventId}/edit`}>Edit</Link>
             <button onClick={() => props.handleDeleteEvent(eventId)}>Delete</button>
           </>
         )}
       </header>
 
+      {props.user && !isOwner && (
+        <button onClick={handleToggleAttend}>
+          {attending ? 'Leave event' : 'Attend event'}
+        </button>
+      )}
+      {!props.user && <p>Please sign in to attend.</p>}
+
+      {isOwner && (
+        <>
+          <h2>Attendees ({attendees.length} out of {event.capacity})</h2>
+          {!attendees.length && <p>No attendees yet.</p>}
+          {attendees.map((attendant) => (
+            <p key={attendant._id}>{attendant.username}</p>
+          ))}
+        </>
+      )}
     </main>
   )
 }
